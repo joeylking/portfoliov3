aac636d94cd5e25278b34e1f820e20d8eb01cf6e
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-26 11:28:11 -0500

    Wireframe, layout, skeleton nav

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const jobs = [
-  "Airman",
-  "waiter",
-  "assembly specialist",
-  "actor",
-  "bartender",
-  "philosophy & international studies student",
-  "receiving exceptions clerk",
-  "physics student",
-  "stay-at-home Dad",
-  "truck driver",
+  'Airman',
+  'waiter',
+  'assembly specialist',
+  'actor',
+  'bartender',
+  'philosophy & international studies student',
+  'warehouse exceptions clerk',
+  'physics student',
+  'stay-at-home Dad',
+  'truck driver',
 ];
-const sequence = document.getElementById("sequence");
-const a = document.getElementById("a");
+const sequence = document.getElementById('sequence');
+const a = document.getElementById('a');
 
 textSequence(0);
 function textSequence(i) {
@@ -19,15 +19,15 @@ function textSequence(i) {
     setTimeout(function () {
       sequence.innerHTML = jobs[i];
       if (i === 0 || i === 2 || i === 3) {
-        a.innerText = "an ";
+        a.innerText = 'an ';
       } else {
-        a.innerText = "a ";
+        a.innerText = 'a ';
       }
       textSequence(++i);
     }, 300); // 1 second (in milliseconds)
   } //if (example.length  == i)
   else {
-    sequence.classList.remove("strike");
-    sequence.innerHTML = "Software Developer";
+    sequence.classList.remove('strike');
+    sequence.innerHTML = 'Software Engineer';
   }
 }
